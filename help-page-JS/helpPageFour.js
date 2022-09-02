d8279403c6ccec8c2e113dd40a48d7002adb422b
AidenValley <jhyjsh953@gmail.com>
2022-09-02 14:22:46 -0700

    feat: helpPageJS sample BOL added

--- help-page-JS/helpPageFour.js
+++ help-page-JS/helpPageFour.js
@@ -1,12 +1,19 @@
 // Global Variables
-const masterBill = document.getElementById('content');
 const home = document.getElementById('homeButton');
 const back = document.getElementById('backButton');
-// const billImage = document.createElement('')
 
-masterBill.addEventListener('click', (e) => {
-  (e.target.textContent === 'Click Me') ? e.target.src = "../images/SAMPLE MASTER BILL OF LADING.png" : e.target.textContent = "Click Me"
+// const billImage = document.createElement('')
+document.querySelectorAll('img').forEach((item) => {
+  item.addEventListener("click", (e) => {
+    const image = e.target.getAttribute('data-src');
+    e.target.setAttribute("src", image);
+  });
 });
+
+
+// masterBill.addEventListener('click', (e) => {
+//   (e.target.textContent === 'Click Me') ? e.target.textContent= img: e.target.textContent = "Click Me"
+// });
 
 back.addEventListener('click', (e) => {
   location.href = "../help-page-html/helpPageThree.html";
